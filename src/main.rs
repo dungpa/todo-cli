ed724dcad7a84d33403d33e031f1f9fb7d8b21a3
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-08-13 15:42:58 +0100

    Return proper exit codes

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,16 @@ mod backend;
 fn main() {
     let args = domain::parse_command();
     match backend::execute(args) {
-        Ok(msgs) => { for msg in msgs { println!("{}", msg); } },
-        Err(err) => { eprintln!("{}", err); }
+        Ok(msgs) => { 
+            for msg in msgs { 
+                println!("{}", msg); 
+            }
+
+            std::process::exit(0); 
+        },
+        Err(err) => { 
+            eprintln!("{}", err); 
+            std::process::exit(1); 
+        }
     };
 }
