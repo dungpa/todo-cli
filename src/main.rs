f4f9e0c70267262be521f6ff4216f2b920a4d359
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-08-13 12:09:02 +0100

    Add a basic domain model

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[derive(Debug)]
+struct AddCommand {
+    todo: String
+}
+
+#[derive(Parser)]
+#[derive(Debug)]
+enum Command {
+    Add(AddCommand),
+}
+
+#[derive(Parser)]
+#[derive(Debug)]
+struct Arguments {
+    /// Command to execute
+    command: String,
+    /// Content of the TODO
+    todo_content: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Command::parse();
+    println!("Arguments, {:?}", args);
 }
